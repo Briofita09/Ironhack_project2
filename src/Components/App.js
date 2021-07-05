e5c2e57a85b311f33e0065d0e9df291d370726fa
Briofita09 <felipe.osorio.santos@hotmail.com>
2021-07-05 17:58:31 -0300

    fazendo a calculadora

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Calculadora from "./Calculadora";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Felipe & Gabriel </h1>
+      <Calculadora />
+    </div>
+  );
+}
+
+export default App;
