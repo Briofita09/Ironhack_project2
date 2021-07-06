87dc343399d58e83d8fd8a959a524f75d46d97e6
Briofita09 <felipe.osorio.santos@hotmail.com>
2021-07-06 17:28:12 -0300

    criado login, singup, api e inicio do resto do app

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,25 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route } from "react-router-dom";
 
+import LogIn from "./Login";
+import Singup from "./Singup";
 import Calculadora from "./Calculadora";
 
 function App() {
   return (
     <div className="App">
-      <h1>Felipe & Gabriel </h1>
-      <Calculadora />
+      <BrowserRouter>
+        <Route
+          exact
+          path="/"
+          render={function (props) {
+            return <LogIn />;
+          }}
+        />
+        <Route exact path="/singup" component={Singup} />
+        <Route exact path="/:id/calculadora" component={Calculadora} />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/SolutionsList.js
@@ -0,0 +1,38 @@
+import React from "react"
+import {Link} from "react-router-dom"
+import axios from "axios";
+
+export default class SolutionsList extends React.Component{
+    state = {
+        solutions = [],
+    }
+
+    componentDidMount = async () => {
+        try{
+            const response = await axios.get("url")
+            this.setState ({solutions: [...response.data]});
+        } catch (err){
+            console.log (err)
+        }
+    }
+
+
+    render(){
+        return(
+            <thead>
+                <tr>
+                    <th>
+                        Solution
+                    </th>
+                    <th>
+
+                    </th>
+                </tr>
+            </thead>
+        )
+    }
+
+}
+
+
+//email, senha, id {solucoes {responsavel, lote, data de fabricacao, validade, solucao}}
