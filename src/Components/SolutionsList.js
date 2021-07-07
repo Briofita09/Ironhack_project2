b3990457a599b4f03e64aba492e4d3aff856be84
Briofita09 <felipe.osorio.santos@hotmail.com>
2021-07-07 12:31:10 -0300

    table renderizando

--- src/Components/SolutionsList.js
+++ src/Components/SolutionsList.js
@@ -23,19 +23,50 @@ export default class SolutionsList extends React.Component {
 
   render() {
     return (
-      <div>
-        <ul>
-          {this.state.solutions.map((solution) => {
+      <table className="table table-dark">
+        <thead>
+          <tr className="table-dark">
+            <th>Nome da Solução/Reagente</th>
+            <th>Concentração</th>
+            <th>Validade</th>
+            <th>Responsavel</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.solutions.map((params) => {
             return (
-              <li>
-                <p>{solution.name}</p>
-              </li>
+              <tr className="table-dark" key={params.name}>
+                <td>{params.name}</td>
+                <td>{params.concentracao}</td>
+                <td>{params.validade}</td>
+                <td>{params.responsavel}</td>
+                <Link
+                  className="me-2"
+                  to={`/:id/edit/${params.name}`}
+                  title="Edit"
+                >
+                  <i className="fas fa-edit"></i>
+                </Link>
+                <Link
+                  className="me-2"
+                  to={`/:id/details/${params.name}`}
+                  title="View details"
+                >
+                  <i className="fas fa-info-circle"></i>
+                </Link>
+                <Link
+                  to={`/:id/delete/${params.name}`}
+                  title="Delete character"
+                  className="text-danger"
+                >
+                  <i className="fas fa-trash-alt"></i>
+                </Link>
+              </tr>
             );
           })}
-        </ul>
-      </div>
+        </tbody>
+      </table>
     );
   }
 }
-
 //email, senha, id {solucoes {responsavel, lote, data de fabricacao, validade, solucao}}
