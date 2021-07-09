d19a8b801cd82a3759be6399a39526fd87ba8fe3
Briofita09 <felipe.osorio.santos@hotmail.com>
2021-07-09 01:03:16 -0300

    closed beta com footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h6>Created by: Felipe Osorio & Gabriel Furquim</h6>
+    </footer>
+  );
+}
+
+export default Footer;
